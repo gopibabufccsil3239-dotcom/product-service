2803abeadf8b4bcb3106e48a0e36a22482b7ecdf
gopibabu <gopibabufccsil3239@gmail.com>
2026-04-11 20:29:53 -0400

    step 1A

---
+++ src/main/java/entity/Product.java
@@ -0,0 +1,22 @@
+package com.microservices.ProductService.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "products")   // ✅ correct table name
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private double price;
+
+    private int stock;
+}
